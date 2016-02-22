8df8faa212fccf03b8b0a9fb92ff457ee76ae5db
Adam Tenderholt <adam_tenderholt@targetdiscovery.com>
2016-02-22 13:31:00 -0800

    PI Task SftpTest: add downloadFromServer cases

--- src/test/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/PeakInvestigator/PeakInvestigatorTaskSftpTest.java
+++ src/test/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/massdetection/PeakInvestigator/PeakInvestigatorTaskSftpTest.java
@@ -14,7 +14,9 @@ import net.sf.mzmine.datamodel.impl.SimpleScan;
 import net.sf.mzmine.modules.rawdatamethods.peakpicking.massdetection.PeakInvestigator.PeakInvestigatorTask.ResponseErrorException;
 import net.sf.mzmine.project.impl.RawDataFileImpl;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
 
@@ -30,14 +32,15 @@ public class PeakInvestigatorTaskSftpTest {
 
 	private ArgumentCaptor<SftpAction> actionCaptor = ArgumentCaptor
 			.forClass(SftpAction.class);
+	@Rule public ExpectedException thrown = ExpectedException.none();
 
 	@Test
-	public void testUploadFileToServerOk() throws IllegalStateException,
+	public void testUploadFileToServer_Ok() throws IllegalStateException,
 			ResponseFormatException, ResponseErrorException,
 			FileNotFoundException, SftpException, JSchException,
 			InterruptedException {
 
-		PeakInvestigatorTask task = createDefaultTask(
+		PeakInvestigatorTask task = createUploadTask(
 				SftpAction.EXAMPLE_RESPONSE_1);
 		task.uploadFileToServer(new File("test.tar"));
 
@@ -52,13 +55,16 @@ public class PeakInvestigatorTaskSftpTest {
 	/**
 	 * Test PeakInvestigatorTask.uploadFileToServer() with HTML response.
 	 */
-	@Test(expected = ResponseFormatException.class)
-	public void testInitializeResponseHTML() throws IllegalStateException,
+	@Test
+	public void testUploadFileToServer_ResponseHTML() throws IllegalStateException,
 			ResponseFormatException, ResponseErrorException,
 			FileNotFoundException, JSchException, SftpException,
 			InterruptedException {
 
-		PeakInvestigatorTask task = createDefaultTask(BaseAction.API_SOURCE);
+		thrown.expect(ResponseFormatException.class);
+		thrown.expectMessage("Server response appears to be HTML/XML");
+
+		PeakInvestigatorTask task = createUploadTask(BaseAction.API_SOURCE);
 		task.uploadFileToServer(new File("test.tar"));
 
 		fail("Should not reach here.");
@@ -67,16 +73,19 @@ public class PeakInvestigatorTaskSftpTest {
 	/**
 	 * Test PeakInvestigatorTask.upoadFileToServer() with real ERROR response.
 	 */
-	@Test(expected = ResponseErrorException.class)
-	public void testInitializeResponseError() throws IllegalStateException,
+	@Test
+	public void testUploadFileToServer_ResponseError() throws IllegalStateException,
 			ResponseFormatException, ResponseErrorException,
 			FileNotFoundException, JSchException, SftpException,
 			InterruptedException {
 
+		thrown.expect(ResponseErrorException.class);
+		thrown.expectMessage("Invalid username or password");
+
 		String response = BaseAction.ERROR_CREDENTIALS
 				.replace("ACTION", "SFTP");
 
-		PeakInvestigatorTask task = createDefaultTask(response);
+		PeakInvestigatorTask task = createUploadTask(response);
 		task.uploadFileToServer(new File("test.tar"));
 
 		fail("Should not reach here.");
@@ -84,16 +93,10 @@ public class PeakInvestigatorTaskSftpTest {
 
 	/**
 	 * Convenience function to build PeakInvestigatorTask that has setup with
-	 * PeakInvestigatorSaaS and RawDataFile mocks.
+	 * RawDataFile mocks.
 	 */
 	private PeakInvestigatorTask createDefaultTask(String response)
 			throws FileNotFoundException, JSchException, SftpException {
-
-		// setup PI Task
-		PeakInvestigatorSaaS vtmx = mock(PeakInvestigatorSaaS.class);
-		doNothing().when(vtmx).putFile(actionCaptor.capture(), anyString(),
-				anyString(), argThat(new IsMonitor()));
-		when(vtmx.executeAction(actionCaptor.capture())).thenReturn(response);
 
 		Scan scan = mock(SimpleScan.class);
 		when(scan.getNumberOfDataPoints()).thenReturn(12345);
@@ -103,10 +106,92 @@ public class PeakInvestigatorTaskSftpTest {
 		when(rawFile.getScan(anyInt())).thenReturn(scan);
 
 		PeakInvestigatorTask task = new PeakInvestigatorTask("test.com",
-				"user", "password", 0).withService(vtmx).withRawDataFile(
-				rawFile);
+				"user", "password", 0).withRawDataFile(rawFile);
 
 		return task;
+	}
+
+	private PeakInvestigatorTask createUploadTask(String response)
+			throws JSchException, SftpException, FileNotFoundException {
+
+		PeakInvestigatorSaaS vtmx = mock(PeakInvestigatorSaaS.class);
+		doNothing().when(vtmx).putFile(actionCaptor.capture(), anyString(),
+				anyString(), argThat(new IsMonitor()));
+		when(vtmx.executeAction(actionCaptor.capture())).thenReturn(response);
+
+		return createDefaultTask(response).withService(vtmx);
+
+	}
+
+	private PeakInvestigatorTask createDownloadTask(String response)
+			throws JSchException, SftpException, FileNotFoundException {
+
+		PeakInvestigatorSaaS vtmx = mock(PeakInvestigatorSaaS.class);
+		doNothing().when(vtmx).getFile(actionCaptor.capture(), anyString(),
+				anyString(), argThat(new IsMonitor()));
+		when(vtmx.executeAction(actionCaptor.capture())).thenReturn(response);
+
+		return createDefaultTask(response).withService(vtmx);
+
+	}
+
+	@Test
+	public void testDownloadFileFromServer_Ok() throws IllegalStateException,
+			ResponseFormatException, ResponseErrorException,
+			FileNotFoundException, SftpException, JSchException,
+			InterruptedException {
+
+		PeakInvestigatorTask task = createDownloadTask(
+				SftpAction.EXAMPLE_RESPONSE_1);
+		task.downloadFileFromServer(new File("test.tar"), new File("test_local.tar"));
+
+		SftpAction action = actionCaptor.getValue();
+		assertEquals(action.getHost(), "peakinvestigator.veritomyx.com");
+		assertEquals(action.getSftpUsername(), "V504");
+		assertEquals(action.getSftpPassword(), "cB34lxCH0anR952gu");
+		assertEquals(action.getPort(), 22022);
+		assertEquals(action.getDirectory(), "/files");
+	}
+
+	/**
+	 * Test PeakInvestigatorTask.downloadFileFromServer() with HTML response.
+	 */
+	@Test
+	public void testDownloadFileFromServer_ResponseHTML()
+			throws IllegalStateException, ResponseFormatException,
+			ResponseErrorException, FileNotFoundException, JSchException,
+			SftpException, InterruptedException {
+
+		thrown.expect(ResponseFormatException.class);
+		thrown.expectMessage("Server response appears to be HTML/XML");
+
+		PeakInvestigatorTask task = createDownloadTask(BaseAction.API_SOURCE);
+		task.downloadFileFromServer(new File("test.tar"), new File(
+				"test_local.tar"));
+
+		fail("Should not reach here.");
+	}
+
+	/**
+	 * Test PeakInvestigatorTask.downloadFileFromServer() with real ERROR response.
+	 */
+	@Test
+	public void testDownloadFileFromServer_ResponseError() throws IllegalStateException,
+			ResponseFormatException, ResponseErrorException,
+			FileNotFoundException, JSchException, SftpException,
+			InterruptedException {
+
+		thrown.expect(ResponseErrorException.class);
+		thrown.expectMessage("Invalid username or password");
+
+		String response = BaseAction.ERROR_CREDENTIALS
+				.replace("ACTION", "SFTP");
+
+		PeakInvestigatorTask task = createDownloadTask(response);
+		task.downloadFileFromServer(new File("test.tar"), new File(
+				"test_local.tar"));
+
+		fail("Should not reach here.");
 	}
 
 	private class IsMonitor extends ArgumentMatcher<SftpProgressMonitor> {
