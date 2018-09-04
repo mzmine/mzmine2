1c7c5920b6c18c7d271c6d908f6bd5337357b1cd
Steffen <Steffen@192.168.0.22>
2018-09-04 21:15:19 +0200

    gui has a chart now, pattern conversion implemented

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/isotopes/isotopepeakscanner/IsotopePeakScannerSetupDialog.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/isotopes/isotopepeakscanner/IsotopePeakScannerSetupDialog.java
@@ -11,8 +11,15 @@ import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JPanel;
 import javax.swing.text.NumberFormatter;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.junit.experimental.theories.DataPoints;
 import net.sf.mzmine.chartbasics.gui.swing.EChartPanel;
+import net.sf.mzmine.datamodel.DataPoint;
+import net.sf.mzmine.datamodel.PolarityType;
 import net.sf.mzmine.parameters.ParameterSet;
 import net.sf.mzmine.parameters.dialogs.ParameterSetupDialog;
 /**
@@ -28,7 +35,8 @@ public class IsotopePeakScannerSetupDialog extends ParameterSetupDialog {
   private JPanel pnlButtons;  // this will contain the navigation
   private JPanel pnlCheckbox; // this will contain the checkbox to enable the preview
   
-  private EChartPanel chartPanel;
+  private EChartPanel pnlChart;
+  private JFreeChart chart;
   private XYPlot plot;
   
   
@@ -62,6 +70,7 @@ public class IsotopePeakScannerSetupDialog extends ParameterSetupDialog {
     pnlButtons = new JPanel(new FlowLayout());
     pnlCheckbox = new JPanel(new FlowLayout());
     
+    
     cbxPreview = new JCheckBox("Preview");
     cbxPreview.addActionListener(this);
     cbxPreview.setSelected(false);;
@@ -85,6 +94,12 @@ public class IsotopePeakScannerSetupDialog extends ParameterSetupDialog {
     btnNextPattern.setPreferredSize(btnNextPattern.getMaximumSize());
     btnNextPattern.setEnabled(cbxPreview.isSelected());
     
+    chart = ChartFactory.createXYLineChart("Isotope pattern preview", "m/z", "Abundance", new XYSeriesCollection(new XYSeries("penis")));
+    pnlChart= new EChartPanel(chart);
+    pnlChart.setEnabled(cbxPreview.isSelected());
+    
+    
+    
     pnlButtons.add(btnPrevPattern);
     pnlButtons.add(txtCurrentPatternIndex);
     pnlButtons.add(btnNextPattern);
@@ -92,6 +107,7 @@ public class IsotopePeakScannerSetupDialog extends ParameterSetupDialog {
     
     pnlPreview.add(pnlCheckbox, BorderLayout.NORTH);
     pnlPreview.add(pnlButtons, BorderLayout.SOUTH);
+    pnlPreview.add(pnlChart, BorderLayout.CENTER);
     
     mainPanel.add(pnlPreview, 3, 0, 0, 0, 1, 1); 
   }
@@ -126,9 +142,48 @@ public class IsotopePeakScannerSetupDialog extends ParameterSetupDialog {
       btnNextPattern.setEnabled(cbxPreview.isSelected());
       btnPrevPattern.setEnabled(cbxPreview.isSelected());
       txtCurrentPatternIndex.setEnabled(cbxPreview.isSelected());
+      
+      if(cbxPreview.isSelected()) {
+        ExtendedIsotopePattern pattern = calculateIsotopePattern();
+        XYSeries[] dataset = convertPatternToXYSeries(pattern);
+        updateChart(dataset);
+      }
     }  
     else if(ae.getSource() == txtCurrentPatternIndex) {
       logger.info(ae.getSource().toString());
     }
+  }
+  
+  private ExtendedIsotopePattern calculateIsotopePattern() {
+    ExtendedIsotopePattern pattern = new ExtendedIsotopePattern();
+    pattern.setUpFromFormula("C16Cl2Br5H20", 0.01, 0.05, 0.02);
+    pattern.applyCharge(1, PolarityType.POSITIVE);
+    return pattern;
+  }
+  
+  private XYSeries[] convertPatternToXYSeries(ExtendedIsotopePattern pattern) {
+    //TODO: how do you read out the parameters from the window?
+    
+    DataPoint[] dp = pattern.getDataPoints();
+    
+    XYSeries[] xypattern = new XYSeries[dp.length];
+    
+    for(int i = 0; i < dp.length; i++) {
+      xypattern[i] = new XYSeries(pattern.getSimplePeakDescription(i));
+      
+      xypattern[i].add(dp[i].getMZ(), 0);
+      xypattern[i].add(dp[i].getMZ(), dp[i].getIntensity());
+    }
+    
+    return xypattern;
+  }
+  
+  private void updateChart(XYSeries[] xypattern) {
+    XYSeriesCollection dataset = new XYSeriesCollection();
+    for(XYSeries xy : xypattern)
+      dataset.addSeries(xy);
+    
+    chart = ChartFactory.createXYLineChart("Isotope pattern preview", "m/z", "Abundance", dataset);
+    pnlChart.setChart(chart);
   }
 }
